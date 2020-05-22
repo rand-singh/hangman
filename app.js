0415134331128abd16fccfd8e2ff099bf322b131
Randeep Singh <randeeprattan90@gmail.com>
2020-05-22 19:53:07 +0100

    Handle letter key presses

--- app.js
+++ app.js
@@ -28,11 +28,46 @@ function displayWord() {
 
   const innerWord = wordEl.innerText.replace(/\n/g, "");
 
-  console.log(wordEl.innerText, innerWord);
   if (innerWord === selectedWord) {
     finalMessage.innerText = "Congratulations! You won!";
     popup.style.display = "flex";
   }
 }
+
+// update the wrong letters array
+function updateWrongLettersEl() {
+  console.log("update wrong letters");
+}
+
+// show notification
+function showNotification() {
+  notification.classList.add("show");
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+// keydown event
+window.addEventListener("keydown", e => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
